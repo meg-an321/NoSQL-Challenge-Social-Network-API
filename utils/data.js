4f08ab5fbebbebe72cbcd6e99d3ef43116d9d3ce
meg <zimnickimegan@yahoo.com>
2024-09-05 21:32:22 -0400

    added data for the user

---
+++ utils/data.js
@@ -0,0 +1,31 @@
+const usernames =
+[
+    'admin',
+    'user1',
+    'user2',
+    'user3',
+    'user4',
+    'user5',
+];
+
+const emails =
+[
+    'hello@gmail.com',
+    'goodbye@gmail.com',
+    'goodevening@yahoo.com',
+    'goodnight@yahoo.com',
+    'gooday@yahoo.com',
+    'afternoon@hotmail.com',
+];
+
+const thoughts =
+[
+    'I am happy',
+    'I am sad',
+    'I am angry',
+    'I am frustrated',
+    'I am confused',
+    'I am tired',
+];
+
+module.exports = { usernames, emails, thoughts };
