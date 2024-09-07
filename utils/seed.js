a1c87ea5364f53e07fed8a1b0515651ef136846b
meg <zimnickimegan@yahoo.com>
2024-09-07 00:35:55 -0400

    updated seed

--- utils/seed.js
+++ utils/seed.js
@@ -1,60 +1,23 @@
+const mongoose = require('mongoose');
 const connection = require('../config/connection');
+const { initialUserData, initialThoughtData } = require('./data');
 const { User, Thought } = require('../models');
-const { usernames, email, thoughts } = require('./data');
 
-connection.on('error', (err) => err);
+async function seedDatabase() {
+	try {
+		await User.deleteMany({});
+		await Thought.deleteMany({});
 
-connection.once('open', async () => {
-  console.log('Connected');
-  // drop all existing data to prep for seeding
-  await User.deleteMany({});
-  await Thought.deleteMany({});
-  console.log('Exiting data dropped');
+		const users = await User.insertMany(initialUserData);
 
-  // arrays so we can populate the arrays then push to the database
-const userThoughts = [];
+		const thoughts = await Thought.insertMany(initialThoughtData);
 
-  // build user data into users array
-  for (let i = 0; i < usernames.length; i++) {
-    const userObj = {
-      username: usernames[i],
-      email: email[i],
-    };
-    const newUser = await User.create(userObj);
-    users.push({
-      _id: newUser._id.toString(),
-      username: newUser.username,
-    });
-  }
+		console.log('Database seeded successfully!');
+	} catch (err) {
+		console.error(err);
+	} finally {
+		connection.close();
+	}
+}
 
-  // build thought data into userThoughts array
-  for (let i = 0; i < thoughts.length; i++) {
-    const thoughtsObj = {
-      username: usernames[i],
-      thoughtText: thoughts[i],
-    };
-    const newThought = await Thought.create(thoughtsObj);
-    userThoughts.push({
-      _id: newThought._id.toString(),
-      username: newThought.username,
-    });
-  }
-
-  // This builds users into the userThoughts array so that the data connects user/userThoughts together
-  for (let i = 0; i < userThoughts.length; i++) {
-    const userId = users.filter(
-      (user) => user.username === userThoughts[i].username
-    );
-    console.log('USER ID', userId);
-    const updatedUser = await User.findOneAndUpdate(
-      { _id: userId[0]._id },
-      { $push: { thoughts: userThoughts[i]._id } },
-      { new: true }
-    );
-    console.log(updatedUser);
-  }
-
-  console.info('Seeding complete');
-
-  process.exit(0);
-});
+seedDatabase();
