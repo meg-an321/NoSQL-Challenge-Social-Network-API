6fcad7bcbc154aa578741a3b2f3d2c3230fe4c03
meg <zimnickimegan@yahoo.com>
2024-09-07 00:58:19 -0400

    updated seed

--- utils/seed.js
+++ utils/seed.js
@@ -1,23 +1,49 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 const connection = require('../config/connection');
 const { initialUserData, initialThoughtData } = require('./data');
 const { User, Thought } = require('../models');
 
-async function seedDatabase() {
-	try {
-		await User.deleteMany({});
-		await Thought.deleteMany({});
+connection.on('error', (err) => err);
 
-		const users = await User.insertMany(initialUserData);
+connection.once('open', async () => {
+	console.log('Seeding database...');
 
-		const thoughts = await Thought.insertMany(initialThoughtData);
+	await User.deleteMany({});
 
-		console.log('Database seeded successfully!');
-	} catch (err) {
-		console.error(err);
-	} finally {
-		connection.close();
-	}
+	await Thought.deleteMany({});
+
+	const users = await User.insertMany(initialUserData);
+
+	const thoughts = await Thought.insertMany(initialThoughtData);
+
+	console.table(users);
+	console.table(thoughts);
+
+	console.log('Database seeded successfully!');
+	connection.close();
+
+	process.exit(0);
 }
+);
 
-seedDatabase();
+
+
+
+// async function seedDatabase() {
+// 	try {
+// 		await User.deleteMany({});
+// 		await Thought.deleteMany({});
+
+// 		const users = await User.insertMany(initialUserData);
+
+// 		const thoughts = await Thought.insertMany(initialThoughtData);
+
+// 		console.log('Database seeded successfully!');
+// 	} catch (err) {
+// 		console.error(err);
+// 	} finally {
+// 		connection.close();
+// 	}
+// }
+
+// seedDatabase();
