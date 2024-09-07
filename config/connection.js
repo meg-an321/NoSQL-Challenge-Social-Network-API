2b01d23da50ecd6d265ba6e21fea5dcea8447131
meg <zimnickimegan@yahoo.com>
2024-09-07 01:01:45 -0400

    referanced mini project

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,11 @@
 const { connect, connection } = require('mongoose');
 
-connect('mongodb://127.0.0.1:27017/social-network-api', );
+const connectionString =
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialNetworkDB';
+
+connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 module.exports = connection;
