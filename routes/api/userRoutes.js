995c17247756cbe12665ad3f12d9e03844ec86bc
meg <zimnickimegan@yahoo.com>
2024-09-07 00:38:13 -0400

    updated

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,21 +1,25 @@
 const router = require('express').Router();
 const {
-  getUsers,
-  getSingleUser,
-  createUser,
-  updateUser,
-  deleteUser,
-  addFriend,
-  removeFriend
+    createUser,
+    getUsers,
+    getUserById,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend
 } = require('../../controllers/userController');
 
-// build route for /api/users
-router.route('/').get(getUsers).post(createUser);
+router.route('/')
+    .get(getUsers)
+    .post(createUser);
 
-// build route for /api/users/:userId
-router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+router.route('/:userId')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser);
 
-// build route for /api/users/:userId/friends/:friendId
-router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
+router.route('/:userId/friends/:friendId')
+    .post(addFriend)
+    .delete(deleteFriend);
 
 module.exports = router;
